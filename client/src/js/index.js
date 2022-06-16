872925dee40c232e6bd3c95d1309bf83ac94f13b
Tomonari-PC\Tomonari <Himenagi1201@gmail.com>
2022-06-16 20:43:56 +0900

    [フロントエンド] React環境構成
    参考URL：https://qiita.com/TsutomuNakamura/items/72d8cf9f07a5a30be048

---
+++ client/src/js/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Layout = props => {
+    return <h1>Welcome!</h1>
+};
+
+const app = document.getElementById('app');
+ReactDOM.render(<Layout />, app);
