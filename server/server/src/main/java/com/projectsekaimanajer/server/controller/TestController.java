77de6fa8e512278067fcebfdf5fb63ba13425b22
Tomonari-PC\Tomonari <Himenagi1201@gmail.com>
2022-06-17 20:41:29 +0900

    [バックエンド] TestControllerにCORSを設定
    参考URL：https://b1san-blog.com/post/spring/spring-cors/

--- server/server/src/main/java/com/projectsekaimanajer/server/controller/TestController.java
+++ server/server/src/main/java/com/projectsekaimanajer/server/controller/TestController.java
@@ -1,9 +1,11 @@
 package com.projectsekaimanajer.server.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin("*")
 public class TestController {
     @RequestMapping("/test")
     public String test() {
