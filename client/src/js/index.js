61946d467cd6b128d8801a88d1fd0042c11a1923
Tomonari-PC\Tomonari <Himenagi1201@gmail.com>
2022-06-17 21:04:12 +0900

    [フロントエンド] サーバーとの連携テスト

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 
 const Layout = props => {
-    return <h1>Welcome!</h1>
+    const [test, setTest] = useState("");
+    useEffect(() => {
+        (async () => {
+            setTest((await axios("http://localhost:8081/test")).data);
+        })();
+    });
+    return <h1>{test}</h1>
 };
 
 const app = document.getElementById('app');
