e72d0a0c615c94ff5f11e7dde102cb4bda037bdc
Tomonari-PC\Tomonari <Himenagi1201@gmail.com>
2022-06-22 22:27:35 +0900

    [バックエンド] MUnitクラスにcreatedTime、updatedTimeを追加

--- server/src/main/java/com/projectsekaimanajer/server/entities/MUnit.java
+++ server/src/main/java/com/projectsekaimanajer/server/entities/MUnit.java
@@ -1,7 +1,11 @@
 package com.projectsekaimanajer.server.entities;
 
+import java.time.LocalDateTime;
+
 public class MUnit {
     public long id;
     public String name;
     public String imageColor;
+    public LocalDateTime createdTime;
+    public LocalDateTime updatedTime;
 }
