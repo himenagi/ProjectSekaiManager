55687a9a316fc80704b98f66b19e345b32e9be28
Tomonari-PC\Tomonari <Himenagi1201@gmail.com>
2022-06-23 00:15:43 +0900

    [バックエンド] JavaDoc設定追加

--- server/src/main/java/com/projectsekaimanajer/server/entities/MUnit.java
+++ server/src/main/java/com/projectsekaimanajer/server/entities/MUnit.java
@@ -2,10 +2,32 @@ package com.projectsekaimanajer.server.entities;
 
 import java.time.LocalDateTime;
 
+/**
+ * m_unitテーブルに対応するエンティティクラス
+ */
 public class MUnit {
+    /**
+     * ユニットID
+     */
     public long id;
+
+    /**
+     * ユニット名
+     */
     public String name;
+
+    /**
+     * イメージカラー
+     */
     public String imageColor;
+
+    /**
+     * 作成日時
+     */
     public LocalDateTime createdTime;
+
+    /**
+     * 更新日時
+     */
     public LocalDateTime updatedTime;
 }
